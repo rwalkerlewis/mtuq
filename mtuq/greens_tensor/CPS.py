b5f167e49a21ffc30407fcdf245935d649daafa8
Robert Walker <langleyreview@gmail.com>
2024-02-16 17:01:36 -0800

    Adding in ability to read GFs from CPS

---
+++ mtuq/greens_tensor/CPS.py
@@ -0,0 +1,110 @@
+
+import obspy
+import numpy as np
+
+from mtuq.greens_tensor.base import GreensTensor as GreensTensorBase
+
+
+class GreensTensor(GreensTensorBase):
+    """
+    CPS Green's tensor object
+
+    Overloads base class with machinery for working with CPS-style
+    Green's functions
+
+    .. note:
+
+      FK Green's functions describe the impulse response of a horizontally-
+      layered medium.  Time series represent vertical, radial, and transverse
+      velocity in units of 10^-20*cm*(dyne-cm)^-1 s^-1
+
+      For the vertical and raidal components, there are four associated time 
+      series. For the tranverse component, there are two associated time
+      series. Thus there are ten independent Green's tensor elements altogether,
+      which is fewer than in the case of a general inhomogeneous medium
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(GreensTensor, self).__init__(*args, **kwargs)
+
+        if 'type:greens' not in self.tags:
+            self.tags += ['type:greens']
+
+        if 'type:velocity' not in self.tags:
+            self.tags += ['type:velocity']
+
+        if 'units:m' not in self.tags:
+            self.tags += ['units:m']
+
+    def _precompute(self):
+        """ Computes NumPy arrays used by get_synthetics
+        """
+        if self.include_mt:
+            self._precompute_mt()
+
+        if self.include_force:
+            self._precompute_force()
+
+    def _precompute_mt(self):
+        """ Recombines CPS time series so they can be used in straightforward
+        linear combination with Mrr,Mtt,Mpp,Mrt,Mrp,Mtp
+        """
+
+        array = self._array
+        phi = np.deg2rad(self.azimuth)
+        _j = 0
+
+        # The formulas below were obtained by reverse engineering FK
+
+        for _i, component in enumerate(self.components):
+            if component == 'Z':
+                ZSS = self.select(channel="ZSS")[0].data
+                ZDS = self.select(channel="ZDS")[0].data
+                ZDD = self.select(channel="ZDD")[0].data
+                ZEX = self.select(channel="ZEX")[0].data
+                array[_i, _j+0, :] = -ZSS/2. * np.cos(2*phi) - ZDD/6. + ZEX/3.
+                array[_i, _j+1, :] = ZSS/2. * np.cos(2*phi) - ZDD/6. + ZEX/3.
+                array[_i, _j+2, :] = ZDD/3. + ZEX/3.
+                array[_i, _j+3, :] = -ZSS * np.sin(2*phi)
+                array[_i, _j+4, :] = -ZDS * np.cos(phi)
+                array[_i, _j+5, :] = -ZDS * np.sin(phi)
+
+            elif component == 'R':
+                RSS = self.select(channel="RSS")[0].data
+                RDS = self.select(channel="RDS")[0].data
+                RDD = self.select(channel="RDD")[0].data
+                REX = self.select(channel="REX")[0].data
+                array[_i, _j+0, :] = -RSS/2. * np.cos(2*phi) - RDD/6. + REX/3.
+                array[_i, _j+1, :] = RSS/2. * np.cos(2*phi) - RDD/6. + REX/3.
+                array[_i, _j+2, :] = RDD/3. + REX/3.
+                array[_i, _j+3, :] = -RSS * np.sin(2*phi)
+                array[_i, _j+4, :] = -RDS * np.cos(phi)
+                array[_i, _j+5, :] = -RDS * np.sin(phi)
+
+            elif component == 'T':
+                TSS = self.select(channel="TSS")[0].data
+                TDS = self.select(channel="TDS")[0].data
+                array[_i, _j+0, :] = -TSS/2. * np.sin(2*phi)
+                array[_i, _j+1, :] = TSS/2. * np.sin(2*phi)
+                array[_i, _j+2, :] = 0.
+                array[_i, _j+3, :] = TSS * np.cos(2*phi)
+                array[_i, _j+4, :] = -TDS * np.sin(phi)
+                array[_i, _j+5, :] = TDS * np.cos(phi)
+
+            else:
+                raise ValueError
+
+        #
+        # FK uses a north-east-down basis convention, while mtuq uses an
+        # up-south-east basis convention, so a permutation is necessary
+        #
+        array_copy = array.copy()
+        array[:, 0, :] = array_copy[:, 2, :]
+        array[:, 1, :] = array_copy[:, 0, :]
+        array[:, 2, :] = array_copy[:, 1, :]
+        array[:, 3, :] = array_copy[:, 4, :]
+        array[:, 4, :] = -array_copy[:, 5, :]
+        array[:, 5, :] = -array_copy[:, 3, :]
+
+    def _precompute_force(self):
+        raise NotImplementedError()
---
+++ mtuq/io/clients/CPS_SAC.py
@@ -0,0 +1,172 @@
+
+import obspy
+import numpy as np
+
+from os.path import basename, exists
+from mtuq.greens_tensor.FK import GreensTensor
+from mtuq.io.clients.base import Client as ClientBase
+from mtuq.util.signal import resample
+from obspy.core import Stream
+from obspy.geodetics import gps2dist_azimuth
+
+
+# FIX FOR CPS
+# An FK simulation outputs 12 SAC files each with filename extensions
+# 0,1,2,3,4,5,6,7,8,9,a,b.  The SAC files ending in .2 and .9 contain
+# only zero data, so we exclude them from the following list.
+# The order of traces in the list is the order in which CAP stores
+# the time series.
+
+# A CPS run where the file96 results are converted to SAC files produces
+
+POSITION = [
+    '8',  '5',           # t
+    '10', '7', '4', '2',   # r
+    '9',  '6', '3', '1',   # z
+]
+
+CHANNELS = [
+    'TSS', 'TDS',
+    'REX', 'RSS', 'RDS', 'RDD',
+    'ZEX', 'ZSS', 'ZDS', 'ZDD',
+]
+
+
+class Client(ClientBase):
+    """  CPS database client
+
+    .. rubric:: Usage
+
+    To instantiate a database client, supply a path or url:
+
+    .. code::
+
+        from mtuq.io.clients.FK_SAC import Client
+        db = Client(path_or_url)
+
+    Then the database client can be used to generate GreensTensors:
+
+    .. code::
+
+        greens_tensors = db.get_greens_tensors(stations, origin)
+
+
+    .. note::
+
+      `GreensTensor`s are obtained by reading precomputed time series from an 
+      CPS directory tree.  
+
+
+
+      Such trees contain SAC files organized by model, 
+      event depth, and event distance, as used by the `Zhu1994`
+      software packages.
+
+    """
+
+    def __init__(self, path_or_url=None, model=None,
+                 include_mt=True, include_force=False):
+
+        if not path_or_url:
+            raise Exception
+
+        if not exists(path_or_url):
+            raise Exception
+
+        if include_force:
+            raise NotImplementedError
+
+        if not model:
+            model = basename(path_or_url)
+
+        # path to CPS directory tree
+        self.path = path_or_url
+
+        # model from which CPS Green's functions were computed
+        self.model = model
+
+        self.include_mt = include_mt
+        self.include_force = include_force
+
+    def get_greens_tensors(self, stations=[], origins=[], verbose=False):
+        """ Extracts Green's tensors from database
+
+        Returns a ``GreensTensorList`` in which each element corresponds to a
+        (station, origin) pair from the given lists
+
+        .. rubric :: Input arguments
+
+        ``stations`` (`list` of `mtuq.Station` objects)
+
+        ``origins`` (`list` of `mtuq.Origin` objects)
+
+        ``verbose`` (`bool`)
+
+        """
+        return super(Client, self).get_greens_tensors(stations, origins, verbose)
+
+    def _get_greens_tensor(self, station=None, origin=None):
+        if station is None:
+            raise Exception("Missing station input argument")
+
+        if origin is None:
+            raise Exception("Missing station input argument")
+
+        traces = []
+
+        distance_in_m, _, _ = gps2dist_azimuth(
+            origin.latitude,
+            origin.longitude,
+            station.latitude,
+            station.longitude)
+
+        # what are the start and end times of the data?
+        t1_new = float(station.starttime)
+        t2_new = float(station.endtime)
+        dt_new = float(station.delta)
+
+        # dep = str(int(round(origin.depth_in_m/1000.)))
+        # dep = str(int(np.ceil(origin.depth_in_m/1000.))).zfill(4)
+        dep = "{:06.1f}".format(np.ceil(origin.depth_in_m/1000.))
+        dep_folder = dep[:-2]
+
+        # dst = str(int(round(distance_in_m/1000.)))
+        # dst = str(int(np.ceil(distance_in_m/1000.))).zfill(4)
+        dst = "{:06.1f}".format(np.ceil(distance_in_m/1000.))
+
+        if self.include_mt:
+
+            for _i, ext in enumerate(CHANNELS):
+                trace = obspy.read('%s/%s/%s%s.%s' %
+                                   (self.path, self.model,
+                                    dep_folder, dep, dst, ext),
+                                   format='sac')[0]
+
+                trace.stats.channel = CHANNELS[_i]
+                trace.stats._component = CHANNELS[_i][0]
+
+                # what are the start and end times of the Green's function?
+                t1_old = float(origin.time)+float(trace.stats.starttime)
+                t2_old = float(origin.time)+float(trace.stats.endtime)
+                dt_old = float(trace.stats.delta)
+                data_old = trace.data
+
+                # resample Green's function
+                data_new = resample(data_old, t1_old, t2_old, dt_old,
+                                    t1_new, t2_new, dt_new)
+                trace.data = data_new
+                # convert from 10^-20 dyne to N^-1
+                trace.data *= 1.e-15
+                trace.stats.starttime = t1_new
+                trace.stats.delta = dt_new
+
+                traces += [trace]
+
+        tags = [
+            'model:%s' % self.model,
+            'solver:%s' % 'CPS',
+        ]
+
+        return GreensTensor(traces=[trace for trace in traces],
+                            station=station, origin=origin, tags=tags,
+                            include_mt=self.include_mt, include_force=self.include_force)
