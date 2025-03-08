87df222fa309dcd0f41f9d80b98b7ba8d777c39b
Robert Walker <rainbowseismic@gmail.com>
2025-03-07 20:41:44 -0800

    Updates but not done

--- mtuq/misfit/waveform/c_ext_PETSc.c
+++ mtuq/misfit/waveform/c_ext_PETSc.c
@@ -3,8 +3,8 @@
 #include <numpy/arrayobject.h>
 #include <numpy/npy_math.h>
 #include <math.h>
-#include <petsc.h>
-
+#include <petscvec.h>
+#include <petscmat.h>
 
 //
 // array access macros
@@ -74,6 +74,20 @@ static PyObject *misfit(PyObject *self, PyObject *args) {
 
   int NSRC, NSTA, NC, NG, NGRP, NPAD;
 
+  // Create PETSc scalars
+  PetscInt ista, ic, it, igrp;
+  PetscScalar value, weight_value;
+
+  // Create PETSc vectors/matrices
+  Vec S, d_d, misfit, final_misfit;
+  Mat G_G, G_d, G_d_w, G_d_w_grp, gg, ww, CC;
+
+
+
+
+
+
+
   // int cc_argmax, j1, j2, nd, NPAD;
   // npy_float64 cc_max, L2_sum, L2_tmp;
 
@@ -104,25 +118,25 @@ static PyObject *misfit(PyObject *self, PyObject *args) {
   NT norm_type = (hybrid_norm == 1) ? NORM_HYBRID : (hybrid_norm == 2) ? NORM_L1 : NORM_L2;
 
   // Initialize PETSc
-  PetscErrorCode ierr;
-  ierr = PetscInitialize(NULL, NULL, NULL, NULL); CHKERRQ(ierr);
+  PetscFunctionBeginUser;
+  PetscCall(PetscInitialize(NULL, NULL, NULL, NULL));
 
 
-  // NSRC = (int) PyArray_SHAPE(sources)[0];
-  // NSTA = (int) PyArray_SHAPE(weights)[0];
-  // NC = (int) PyArray_SHAPE(weights)[1];
-  // NG = (int) PyArray_SHAPE(sources)[1];
-  // NGRP = (int) PyArray_SHAPE(groups)[0];
+  NSRC = (int) PyArray_SHAPE(sources)[0];
+  NSTA = (int) PyArray_SHAPE(weights)[0];
+  NC = (int) PyArray_SHAPE(weights)[1];
+  NG = (int) PyArray_SHAPE(sources)[1];
+  NGRP = (int) PyArray_SHAPE(groups)[0];
 
-  // NPAD = (int) NPAD1+NPAD2+1;
+  NPAD = (int) NPAD1+NPAD2+1;
 
-  // if (debug_level>1) {
-  //   printf(" number of sources:  %d\n", NSRC);
-  //   printf(" number of stations:  %d\n", NSTA);
-  //   printf(" number of components:  %d\n", NC);
-  //   printf(" number of Green's functions:  %d\n\n", NG);
-  //   printf(" number of component groups:  %d\n", NGRP);
-  // }
+  if (debug_level>1) {
+    printf(" number of sources:  %d\n", NSRC);
+    printf(" number of stations:  %d\n", NSTA);
+    printf(" number of components:  %d\n", NC);
+    printf(" number of Green's functions:  %d\n\n", NG);
+    printf(" number of component groups:  %d\n", NGRP);
+  }
 
   // // Convert NumPy arrays to raw data pointers
   PetscScalar *d_d_data = (PetscScalar *) PyArray_DATA(data_data);
@@ -133,72 +147,126 @@ static PyObject *misfit(PyObject *self, PyObject *args) {
   PetscScalar *Q_data = (PetscScalar *) PyArray_DATA(groups);
 
 
-  // Create PETSc vectors/matrices
-  Vec S, d_d, misfit, cc;
-  Mat G_G, G_d;
+  // Assign to PETSc structures
+  // PetscCall(VecCreateMPIWithArray(PETSC_COMM_WORLD, 1, NSRC * NG, PETSC_DECIDE, S_data, &S));
+  PetscCall(VecCreateMPIWithArray(PETSC_COMM_WORLD, 1, NSTA * NC, PETSC_DECIDE, d_d_data, &d_d));
+  PetscCall(MatCreateDense(PETSC_COMM_WORLD, PETSC_DECIDE, PETSC_DECIDE, NSTA * NC * NG, NG, G_G_data, &G_G));
+  PetscCall(MatCreateDense(PETSC_COMM_WORLD, PETSC_DECIDE, PETSC_DECIDE, NSTA * NC * NPAD, NG, G_d_data, &G_d));
+  PetscCall(MatCreateDense(PETSC_COMM_WORLD, PETSC_DECIDE, PETSC_DECIDE, NSRC, NG, S_data, &s));
+  PetscCall(VecCreateMPI(PETSC_COMM_WORLD, PETSC_DECIDE, NSRC * NPAD, &cc));
+  PetscCall(VecCreateMPI(PETSC_COMM_WORLD, PETSC_DECIDE, NSRC, &misfit));
+  PetscCall(VecCreateMPI(PETSC_COMM_WORLD, PETSC_DECIDE, NSRC, &final_misfit));
 
-  ierr = VecCreateMPIWithArray(PETSC_COMM_WORLD, 1, NSRC * NG, PETSC_DECIDE, S_data, &S); CHKERRQ(ierr);
-  ierr = VecCreateMPIWithArray(PETSC_COMM_WORLD, 1, NSTA * NC, PETSC_DECIDE, d_d_data, &d_d); CHKERRQ(ierr);
-  ierr = MatCreateDense(PETSC_COMM_WORLD, PETSC_DECIDE, PETSC_DECIDE, NSTA * NC * NG, NG, G_G_data, &G_G); CHKERRQ(ierr);
-  ierr = MatCreateDense(PETSC_COMM_WORLD, PETSC_DECIDE, PETSC_DECIDE, NSTA * NC * NG, NPTS, G_d_data, &G_d); CHKERRQ(ierr);
-  ierr = VecCreateMPI(PETSC_COMM_WORLD, PETSC_DECIDE, NSRC * NPTS, &cc); CHKERRQ(ierr);
-  ierr = VecCreateMPI(PETSC_COMM_WORLD, PETSC_DECIDE, NSRC, &misfit); CHKERRQ(ierr);
+  // Create a PetscMat to populate weights
+  PetscCall(MatCreateDense(PETSC_COMM_WORLD, PETSC_DECIDE, PETSC_DECIDE, 1, NSTA*NC*NPAD, &ww));
 
-
-  // Step 1: Compute cross-correlation cc = G_d * S
-  ierr = MatMult(G_d, S, cc); CHKERRQ(ierr);
-
-  // Step 2: Find optimal time shift for each source
-  PetscInt its[NSRC];
-  ierr = VecStrideMax(cc, NPTS, NULL, its); CHKERRQ(ierr);
-
-  // Step 3: Extract the corresponding rows from G_G
-  Mat G_G_opt;
-  ierr = MatGetSubMatrix(G_G, its, NULL, MAT_INITIAL_MATRIX, &G_G_opt); CHKERRQ(ierr);
-
-  // Step 4: Compute misfit
-  Vec S_GG_S, S_Gd;
-  ierr = VecDuplicate(S, &S_GG_S); CHKERRQ(ierr);
-  ierr = VecDuplicate(S, &S_Gd); CHKERRQ(ierr);
-
-  ierr = MatMult(G_G_opt, S, S_GG_S); CHKERRQ(ierr);
-  ierr = MatMult(G_d, S, S_Gd); CHKERRQ(ierr);
-  ierr = VecScale(S_Gd, -2.0); CHKERRQ(ierr);
-  ierr = VecWAXPY(misfit, 1.0, S_GG_S, d_d); CHKERRQ(ierr);
-  ierr = VecAXPY(misfit, 1.0, S_Gd); CHKERRQ(ierr);
-
-
-  // Step 5: Apply norm selection
-  if (norm_type == NORM_L1) {
-      ierr = VecAbs(misfit); CHKERRQ(ierr);  // L1 norm
-  } else if (norm_type == NORM_HYBRID) {
-      ierr = VecSqrtAbs(misfit); CHKERRQ(ierr);  // Hybrid L1-L2 norm
-  } else {
-      // L2 norm (default)
-      // No additional operation needed since we directly computed the squared residuals.
+  // Populate ww matrix
+  for (ista = 0; ista < NSTA; ista++) {
+    for (ic = 0; ic < NC; ic++) {
+        for (it = 0; it < NPAD; it++) {
+            col = (ista * NC * NPAD) + (ic * NPAD) + it;
+            weight_value = weights[ic][ista];  // Get weight from weights[nc, nsta]
+            PetscCall(MatSetValue(W, row, col, weight_value, INSERT_VALUES));
+        }
+    }
   }
 
-    // Convert PETSc misfit vector to NumPy array
-    double *misfit_data;
-    ierr = VecGetArray(misfit, &misfit_data); CHKERRQ(ierr);
-    npy_intp dims[1] = {NSRC};
-    PyObject *result = PyArray_SimpleNewFromData(1, dims, NPY_DOUBLE, misfit_data);
+  // Multiply to assign weights
+  PetscCall(MatMatMult(G_d, ww, MAT_INITIAL_MATRIX, PETSC_DECIDE, G_d_w));
 
-    // Cleanup PETSc objects
-    ierr = VecRestoreArray(misfit, &misfit_data); CHKERRQ(ierr);
-    ierr = MatDestroy(&G_G); CHKERRQ(ierr);
-    ierr = MatDestroy(&G_G_opt); CHKERRQ(ierr);
-    ierr = MatDestroy(&G_d); CHKERRQ(ierr);
-    ierr = VecDestroy(&S); CHKERRQ(ierr);
-    ierr = VecDestroy(&d_d); CHKERRQ(ierr);
-    ierr = VecDestroy(&cc); CHKERRQ(ierr);
-    ierr = VecDestroy(&misfit); CHKERRQ(ierr);
-    ierr = VecDestroy(&S_GG_S); CHKERRQ(ierr);
-    ierr = VecDestroy(&S_Gd); CHKERRQ(ierr);
+  // Create a PetscMat to use to zero components not in group
+  PetscCall(MatCreateDense(PETSC_COMM_WORLD, PETSC_DECIDE, PETSC_DECIDE, 1, NSTA*NC*NPAD, &gg));
 
-    ierr = PetscFinalize(); CHKERRQ(ierr);
+  // Perform action on time shift groups
+  for (igrp = 0; igrp < NGRP; igrp++) {
+ 
+    // Zero out components not included in this group - Maybe just pull out sparse matrix without those values?
+    // Populate the vector
+    for (ista = 0; ista < NSTA; ista++) {
+      for (ic = 0; ic < NC; ic++) {
+        // Check if this component is zeroed out based on the group matrix
+        PetscBool is_zero = PETSC_TRUE;
+        if (groups[igrp][ic] != 0) {
+          is_zero = PETSC_FALSE;
+          break;
+        }
+        for (it = 0; it < NPAD; it++) {
+          PetscInt idx = (ista * NC * NPAD) + (ic * NPAD) + it;
+          value = is_zero ? 0.0 : 1.0;  // Assign 0 if it should be zeroed, otherwise 1.0
+          PetscCall(MatSetValue(gg, 0, idx, value, INSERT_VALUES));
+        }
+      }
+    } 
+    // Finalize assembly of the vector
+    PetscCall(VecAssemblyBegin(gg));
+    PetscCall(VecAssemblyEnd(gg));
 
-    return result;
+    // Multiply to zero components not in group
+    PetscCall(MatMatMult(G_d_w, gg, MAT_INITIAL_MATRIX, PETSC_DECIDE, G_d_w_grp));
+
+    // Step 1: Compute cross-correlation CC[NSTA*NC*NPAD,NSRC] = G_d[NSTA*NC*NPAD,NG] * S[NG,NSRC] 
+    PetscCall(MatMatMult(G_d_w_grp, S, MAT_INITIAL_MATRIX, PETSC_DECIDE, CC));
+  
+    // Step 2: Find optimal time shift for each source
+
+    // Unravel CC to find optimal NPAD value for each station and component.
+
+
+    PetscInt its[NSRC];
+    PetscCall(VecStrideMax(cc, NPTS, NULL, its));
+
+    // Step 3: Extract the corresponding rows from G_G
+    Mat G_G_opt;
+    PetscCall(MatGetSubMatrix(G_G, its, NULL, MAT_INITIAL_MATRIX, &G_G_opt));
+
+    // Step 4: Compute misfit
+    Vec S_GG_S, S_Gd;
+    PetscCall(VecDuplicate(S, &S_GG_S));
+    PetscCall(VecDuplicate(S, &S_Gd));
+
+    PetscCall(MatMult(G_G_opt, S, S_GG_S));
+    PetscCall(MatMult(G_d, S, S_Gd));
+    PetscCall(VecScale(S_Gd, -2.0));
+    PetscCall(VecWAXPY(misfit, 1.0, S_GG_S, d_d));
+    PetscCall(VecAXPY(misfit, 1.0, S_Gd));
+
+
+    // Step 5: Apply norm selection
+    if (norm_type == NORM_L1) {
+        PetscCall(VecAbs(misfit); // L1 norm
+    } else if (norm_type == NORM_HYBRID) {
+        PetscCall(VecSqrtAbs(misfit); // Hybrid L1-L2 norm
+    } else {
+        // L2 norm (default)
+        // No additional operation needed since we directly computed the squared residuals.
+    }
+  }
+  
+  // Step 6: Combine into final vector.
+  PetscCall(VecAXPY(final_misfit, 1.0, misfit));
+
+
+  // Convert PETSc misfit vector to NumPy array
+  double *misfit_data;
+  PetscCall(VecGetArray(misfit, &misfit_data));
+  npy_intp dims[1] = {NSRC};
+  PyObject *result = PyArray_SimpleNewFromData(1, dims, NPY_DOUBLE, misfit_data);
+
+  // Cleanup PETSc objects
+  PetscCall(VecRestoreArray(misfit, &misfit_data));
+  PetscCall(MatDestroy(&G_G));
+  PetscCall(MatDestroy(&G_G_opt));
+  PetscCall(MatDestroy(&G_d));
+  PetscCall(VecDestroy(&S));
+  PetscCall(VecDestroy(&d_d));
+  PetscCall(VecDestroy(&cc));
+  PetscCall(VecDestroy(&misfit));
+  PetscCall(VecDestroy(&S_GG_S));
+  PetscCall(VecDestroy(&S_Gd));
+
+  PetscCall(PetscFinalize());
+
+  return result;
 }
 
 // Register method with Python
